6e7229bcbe2716d5b47add0d1b3260e45c805bc5
David Szorad <david.szorad@eset.sk>
2021-10-29 11:05:57 +0200

    Add additional properties to websitearchive

--- Core/Database/Entities/WebsiteArchive.cs
+++ Core/Database/Entities/WebsiteArchive.cs
@@ -6,8 +6,12 @@ namespace Core.Database.Entities
     {
         public long WebsiteArchiveId { get; set; }
         public Guid PublicId { get; set; }
+        public string ShortId { get; set; }
         public string Name { get; set; }
-        
+        public string Description { get; set; }
+        public string SourceUrl { get; set; }
+        public string ArchiveUrl { get; set; }
+
         public Enums.ArchiveType ArchiveTypeId { get; set; }
         public ArchiveType ArchiveType { get; set; }
 
