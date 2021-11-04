95641c8ccd2724c9d01482c2715da3f87b0c2bdf
davidszorad <david.szorad@live.com>
2021-11-04 20:23:06 +0100

    Initial commit

--- Core/Scheduler/SchedulerService.cs
+++ Core/Scheduler/SchedulerService.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Core.Database;
 
 namespace Core.Scheduler
 {
     public class SchedulerService : ISchedulerService
     {
+        private readonly AreawaDbContext _areawaDbContext;
+
+        public SchedulerService(AreawaDbContext areawaDbContext)
+        {
+            _areawaDbContext = areawaDbContext;
+        }
+        
         public async Task<Guid> CreateAsync(CreateArchivedWebsiteCommand command, CancellationToken cancellationToken = default)
         {
+            
+            
+            
+            
+            _areawaDbContext.WebsiteArchive.Add()
+            
+            
             // TODO: add to queue
             
             
