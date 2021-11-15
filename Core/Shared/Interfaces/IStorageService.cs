abc7c9600e2075c8ce275bcdbc2bc943f903ade0
David Szorad <david.szorad@eset.sk>
2021-11-15 10:04:12 +0100

    Add IStorageService.cs

---
+++ Core/Shared/Interfaces/IStorageService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Shared;
+
+public interface IStorageService
+{
+    Task<string> CreateAsync(string folder, string file, CancellationToken cancellationToken = default);
+    Task DeleteFileAsync(string folder, string fileName, CancellationToken cancellationToken = default);
+    Task DeleteFolderAsync(string folder, CancellationToken cancellationToken = default);
+}
