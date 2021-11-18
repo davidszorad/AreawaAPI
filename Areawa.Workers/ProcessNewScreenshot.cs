ee92a4d42e1cc069cdea9305b65cc22ec99442b5
davidszorad <david.szorad@live.com>
2021-11-18 07:53:36 +0100

    Initial commit

---
+++ Areawa.Workers/ProcessNewScreenshot.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Areawa.Workers;
+
+public static class ProcessNewScreenshot
+{
+    [Function("ProcessNewScreenshot")]
+    public static void Run([QueueTrigger("myqueue", Connection = "")] string myQueueItem,
+        FunctionContext context)
+    {
+        var logger = context.GetLogger("ProcessNewScreenshot");
+        logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        
+    }
+}
