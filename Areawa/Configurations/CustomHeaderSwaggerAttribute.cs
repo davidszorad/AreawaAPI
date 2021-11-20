ce980a3b6ff4ee71d350444b6bdf2d597ff45a46
davidszorad <david.szorad@live.com>
2021-11-20 21:39:22 +0100

    Change api key parsing

--- Areawa/Configurations/CustomHeaderSwaggerAttribute.cs
+++ Areawa/Configurations/CustomHeaderSwaggerAttribute.cs
@@ -34,11 +34,10 @@ public static class HeaderParser
     {
         apiUserPublicId = Guid.Empty;
         var apiKey = request.Headers.FirstOrDefault(x => x.Key.Equals("X-ApiKey"));
-        if ((apiKey.Key, apiKey.Value) == default)
+        if ((apiKey.Key, apiKey.Value) != default && Guid.TryParse(apiKey.Value, out apiUserPublicId))
         {
-            return false;
+            return true;
         }
-        apiUserPublicId = Guid.Parse(apiKey.Value);
-        return true;
+        return false;
     }
 }
