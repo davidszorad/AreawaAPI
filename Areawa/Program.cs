6ca4a90da20f4be8b1f91e6b001ace6b415ba687
davidszorad <david.szorad@live.com>
2021-11-22 22:42:33 +0100

    Add swagger to prod env

--- Areawa/Program.cs
+++ Areawa/Program.cs
@@ -57,6 +57,9 @@ namespace Areawa
                 //?? app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //?? app.UseHsts();
+                
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Areawa v1"));
             }
 
             app.UseHttpsRedirection();
