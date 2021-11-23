b33b58bb0828f341a6111ceb06196c1bf335a774
davidszorad <david.szorad@live.com>
2021-11-23 21:17:33 +0100

    add new migration

---
+++ Core/Database/Migrations/20211123194125_UpdateEntitesWithApiUser.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Database.Migrations
+{
+    public partial class UpdateEntitesWithApiUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WebsiteArchive_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "WebsiteArchive");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ApiUserId",
+                schema: "areawa",
+                table: "Category",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryGroup_ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup",
+                column: "ApiUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_ApiUserId",
+                schema: "areawa",
+                table: "Category",
+                column: "ApiUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Category_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "Category",
+                column: "ApiUserId",
+                principalSchema: "identity",
+                principalTable: "ApiUser",
+                principalColumn: "ApiUserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryGroup_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup",
+                column: "ApiUserId",
+                principalSchema: "identity",
+                principalTable: "ApiUser",
+                principalColumn: "ApiUserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WebsiteArchive_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "WebsiteArchive",
+                column: "ApiUserId",
+                principalSchema: "identity",
+                principalTable: "ApiUser",
+                principalColumn: "ApiUserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Category_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "Category");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryGroup_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WebsiteArchive_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "WebsiteArchive");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryGroup_ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Category_ApiUserId",
+                schema: "areawa",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "ApiUserId",
+                schema: "areawa",
+                table: "CategoryGroup");
+
+            migrationBuilder.DropColumn(
+                name: "ApiUserId",
+                schema: "areawa",
+                table: "Category");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WebsiteArchive_ApiUser_ApiUserId",
+                schema: "areawa",
+                table: "WebsiteArchive",
+                column: "ApiUserId",
+                principalSchema: "identity",
+                principalTable: "ApiUser",
+                principalColumn: "ApiUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
