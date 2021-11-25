c481066d34621520f3fd8876ed099765b91beab0
David Szorad <david.szorad@eset.sk>
2021-11-25 15:56:03 +0100

    add unit tests

---
+++ Awa.UnitTests/FileSystemServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Awa.UnitTests;
+
+[TestFixture]
+public class FileSystemServiceTests
+{
+    private static string _fileName = "testFile";
+    
+    [TearDown]
+    public void TearDown()
+    {
+        FileSystemService.DeleteFile(_fileName);
+    }
+    
+    [Test]
+    public async Task SaveTextAsync_SaveNewGuid_GuidIsSavedAndCanBeRead()
+    {
+        var newGuid = Guid.NewGuid();
+
+        await FileSystemService.SaveTextAsync(newGuid.ToString(), _fileName);
+        var savedText = await FileSystemService.ReadTextAsync(_fileName);
+        var readGuid = Guid.Parse(savedText);
+
+        Assert.That(readGuid, Is.EqualTo(newGuid));
+    }
+}
---
+++ Awa/FileSystemService/FileSystemService.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Awa.UnitTests")]
+
+namespace Awa;
+
+internal static class FileSystemService
+{
+    public static string GetProfileFolder()
+    {
+        var rootFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var directory = Path.Combine(rootFolderPath, "areawa");
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        return directory;
+    }
+    
+    public static async Task SaveTextAsync(string text, string fileName, CancellationToken cancellationToken = default)
+    {
+        await File.WriteAllTextAsync(Path.Combine(GetProfileFolder(), $"{fileName}.txt"), text, cancellationToken);
+    }
+    
+    public static async Task<string> ReadTextAsync(string fileName, CancellationToken cancellationToken = default)
+    {
+        return await File.ReadAllTextAsync(Path.Combine(GetProfileFolder(), $"{fileName}.txt"), cancellationToken);
+    }
+
+    public static void DeleteFile(string fileName)
+    {
+        File.Delete(Path.Combine(GetProfileFolder(), $"{fileName}.txt"));
+    }
+}
