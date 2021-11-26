4d4750bf4e56152db533b6d6184c0ba340f977d9
David Szorad <david.szorad@eset.sk>
2021-11-26 11:54:09 +0100

    add temp controller actions

--- Areawa/Controllers/WebsiteArchiveController.cs
+++ Areawa/Controllers/WebsiteArchiveController.cs
@@ -4,6 +4,8 @@ using Core.Scheduler;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using Core.Shared;
+using Microsoft.AspNetCore.Http;
 
 namespace Areawa.Controllers;
 
@@ -15,17 +17,20 @@ public class WebsiteArchiveController : ControllerBase
     private readonly IReaderService _readerService;
     private readonly ISchedulerService _schedulerService;
     private readonly IApiKeyValidator _apiKeyValidator;
+    private readonly IStorageService _storageService;
 
     public WebsiteArchiveController(
         ILogger<WebsiteArchiveController> logger,
         IReaderService readerService,
         ISchedulerService schedulerService,
-        IApiKeyValidator apiKeyValidator)
+        IApiKeyValidator apiKeyValidator,
+        IStorageService storageService)
     {
         _logger = logger;
         _readerService = readerService;
         _schedulerService = schedulerService;
         _apiKeyValidator = apiKeyValidator;
+        _storageService = storageService;
     }
 
     [HttpPost("search")]
@@ -81,13 +86,18 @@ public class WebsiteArchiveController : ControllerBase
     }
 
     [HttpPost("upload")]
-    public async Task<IActionResult> UploadScreenshot()
+    [RequestSizeLimit(20_000_000)] //default 30 MB (~28.6 MiB) max request body size limit -- https://github.com/aspnet/Announcements/issues/267
+    public async Task<IActionResult> UploadScreenshot(IFormFile file)
     {
         var apiKeyValidatorResult = await _apiKeyValidator.ValidateAsync(Request);
         if (!apiKeyValidatorResult.isValid)
         {
             return BadRequest();
         }
+
+        var screenshotStream = file.OpenReadStream();
+        
+        var archivePath = await _storageService.UploadAsync(screenshotStream, "20211126-test", "subor.pdf");
 
         return Ok();
     }
