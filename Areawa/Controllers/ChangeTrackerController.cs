9bf123c645ce2b5ff0d79b88148fbab48f752650
davidszorad <david.szorad@live.com>
2021-12-02 20:20:22 +0100

    add user validation

--- Areawa/Controllers/ChangeTrackerController.cs
+++ Areawa/Controllers/ChangeTrackerController.cs
@@ -7,15 +7,25 @@ namespace Areawa.Controllers;
 public class ChangeTrackerController : ControllerBase
 {
     private readonly IChangeTrackerService _changeTrackerService;
+    private readonly IApiKeyValidator _apiKeyValidator;
 
-    public ChangeTrackerController(IChangeTrackerService changeTrackerService)
+    public ChangeTrackerController(
+        IChangeTrackerService changeTrackerService,
+        IApiKeyValidator apiKeyValidator)
     {
         _changeTrackerService = changeTrackerService;
+        _apiKeyValidator = apiKeyValidator;
     }
     
     [HttpPost("source-preview")]
     public async Task<IActionResult> GetSourcePreview([FromBody] SourcePreviewCommand command)
     {
+        var apiKeyValidatorResult = await _apiKeyValidator.ValidateAsync(Request);
+        if (!apiKeyValidatorResult.isValid)
+        {
+            return BadRequest();
+        }
+        
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
