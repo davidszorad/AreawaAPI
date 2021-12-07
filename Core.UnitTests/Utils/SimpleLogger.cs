a635707f384d05ba87d2e5c6dc6762bc8b050373
David Szorad <david.szorad@eset.sk>
2021-12-07 23:31:47 +0100

    Add utils to unit tests

---
+++ Core.UnitTests/Utils/SimpleLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Core.UnitTests.Utils;
+
+public class SimpleLoggerProvider : ILoggerProvider
+{
+    public void Dispose()
+    {
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new SimpleLogger(categoryName);
+    }
+}
+
+public class SimpleLogger : ILogger
+{
+    private readonly string _name;
+    public SimpleLogger(string name) => _name = name;
+    
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        if (formatter == null)
+        {
+            throw new ArgumentNullException(nameof(formatter));
+        }
+
+        var message = formatter(state, exception);
+
+        if (string.IsNullOrEmpty(message))
+        {
+            return;
+        }
+
+        message = $"{ logLevel }: {message}";
+
+        if (exception != null)
+        {
+            message += Environment.NewLine + Environment.NewLine + exception;
+        }
+        
+        Console.WriteLine($"[{_name}]: {message}");
+    }
+    
+    public IDisposable BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+}
