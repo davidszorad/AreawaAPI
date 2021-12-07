99defdb77f5dd8d9b29302c830e33ac96a8e7a61
David Szorad <david.szorad@eset.sk>
2021-12-07 23:55:13 +0100

    Add EntityFrameworkTests - ExecuteCustomQueryTest

---
+++ Core.UnitTests/EntityFrameworkTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Configuration;
+using Core.Database;
+using Core.UnitTests.Utils;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace Core.UnitTests;
+
+public class EntityFrameworkTests
+{
+    [Test]
+    public async Task ExecuteCustomQueryTest()
+    {
+        var loggerFactory = LoggerFactory.Create(log =>
+        {
+            log.AddProvider(new SimpleLoggerProvider());
+            log.AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information);
+        });
+
+        var builder = new DbContextOptionsBuilder<AreawaDbContext>()
+            .UseSqlServer(ConfigStore.GetDbConnectionString())
+            .UseLoggerFactory(loggerFactory);
+        var context = new AreawaDbContext(builder.Options);
+        
+        // add any query on top of context that will be executed on SQL server DB
+        
+        await Task.FromResult(0);
+    }
+}
