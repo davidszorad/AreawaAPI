e37f2737f54595cf4fdeeb1465dc585ced8bb159
David Szorad <david.szorad@eset.sk>
2021-12-27 11:25:54 +0100

    update db

--- Core/Database/Migrations/20211227102357_UpdateWatchDogWithScanCount.Designer.cs
+++ Core/Database/Migrations/20211227102357_UpdateWatchDogWithScanCount.Designer.cs
@@ -4,6 +4,7 @@ using Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Core.Database.Migrations
 {
     [DbContext(typeof(AreawaDbContext))]
-    partial class AreawaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211227102357_UpdateWatchDogWithScanCount")]
+    partial class UpdateWatchDogWithScanCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Core/Database/Migrations/20211227102357_UpdateWatchDogWithScanCount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Database.Migrations
+{
+    public partial class UpdateWatchDogWithScanCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ScanCount",
+                schema: "areawa",
+                table: "WatchDog",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScanCount",
+                schema: "areawa",
+                table: "WatchDog");
+        }
+    }
+}
