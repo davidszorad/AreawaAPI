5c6a2da99435da31c202f8d10e27fbd9255b7ec0
David Szorad <david.szorad@outlook.com>
2022-01-12 18:25:09 +0100

    Replace not allowed chars

---
+++ Core/WebsiteArchiveCreator/Extensions/StringReplaceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.WebsiteArchiveCreator;
+
+internal static class StringReplaceExtensions
+{
+    public static string ReplaceNotAllowedChars(this string text)
+    {
+        return text
+            .Trim()
+            .Replace(" ", "-")
+            .Replace("#", "")
+            .Replace("@", "")
+            .Replace("~", "")
+            .Replace("!", "")
+            .Replace("?", "")
+            .Replace(":", "")
+            .Replace(";", "")
+            .Replace("^", "")
+            .Replace("&", "")
+            .Replace("%", "")
+            .Replace("$", "")
+            .Replace(",", "");
+    }
+}
