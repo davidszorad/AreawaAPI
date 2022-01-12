04bf26bd51925395bb61e7105b6f4355ee9a29e4
David Szorad <david.szorad@eset.sk>
2022-01-12 10:29:03 +0100

    Add unit test

--- Awa/CliCommands/CliScreenshotCommand.cs
+++ Awa/CliCommands/CliScreenshotCommand.cs
@@ -1,25 +1,19 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
-using System.Text.RegularExpressions;
-using Configuration;
 using Core.Shared;
-using Core.WebsiteArchiveCreator;
-using Domain.Enums;
 using Infrastructure;
 
 namespace Awa;
 
 internal class CliScreenshotCommand
 {
-    private readonly IScreenshotCreator _screenshotCreator;
-    private readonly HttpService _httpService;
+    private readonly ScreenshotService _screenshotService;
     private readonly Spinner _spinner;
     
     public CliScreenshotCommand(IScreenshotCreator screenshotCreator)
     {
-        _screenshotCreator = screenshotCreator;
-        _httpService = new HttpService();
+        _screenshotService = new ScreenshotService(screenshotCreator);
         _spinner = new Spinner();
     }
     
@@ -48,7 +42,7 @@ internal class CliScreenshotCommand
 
         return command;
     }
-    
+
     private async Task TakeScreenshotAsync(string url, string name, string description, bool pdf, bool image, IConsole console)
     {
         if (!RuntimeInfoService.IsMac())
@@ -59,69 +53,9 @@ internal class CliScreenshotCommand
         var source = new CancellationTokenSource();
         CancellationToken cancellationToken = source.Token;
 
-        var apiKey = await FileSystemService.ReadTextAsync(ConfigurationConstants.FileNameWithApiKey, cancellationToken);
-
-        Stream stream;
-        if (RuntimeInfoService.IsMac())
-        {
-            stream = await _screenshotCreator.TakeScreenshotStreamAsync(url, ArchiveType.Pdf, _spinner.Start, cancellationToken);   
-        }
-        else
-        {
-            stream = await _screenshotCreator.TakeScreenshotStreamAsync(url, ArchiveType.Pdf, cancellationToken);
-        }
-
-        if (!TryGetArchiveType(pdf, image, console, out var archiveType) || !archiveType.HasValue || !IsValidUrl(url))
-        {
-            await stream.DisposeAsync();
-            _spinner.Stop();
-            console.Error.WriteLine($"Error");
-        }
+        var response = await _screenshotService.TakeScreenshotAsync(url, name, description, pdf, image, _spinner.Start, cancellationToken);
         
-        var createArchivedWebsiteCommand = new CreateArchivedWebsiteCommand
-        {
-            Name = name,
-            ArchiveType = archiveType ?? ArchiveType.Pdf,
-            Description = description,
-            SourceUrl = url
-        };
-            
-        var result = await _httpService.PostAsync(apiKey, stream, createArchivedWebsiteCommand, cancellationToken);
-            
-        await stream.DisposeAsync();
         _spinner.Stop();
-        console.Out.WriteLine($"Done. { result }");
-    }
-
-    private bool TryGetArchiveType(bool isPdf, bool isImage, IConsole console, out ArchiveType? archiveType)
-    {
-        if (!isPdf && !isImage)
-        {
-            archiveType = ArchiveType.Pdf;
-            return true;
-        }
-
-        if (isPdf)
-        {
-            archiveType = ArchiveType.Pdf;
-            return true;
-        }
-
-        if (isImage)
-        {
-            archiveType = ArchiveType.Png;
-            return true;
-        }
-        
-        console.Error.WriteLine("Choose either PDF or IMAGE.");
-        archiveType = null;
-        return false;
-    }
-    
-    private bool IsValidUrl(string url)
-    {
-        string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
-        Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        return Rgx.IsMatch(url);
+        console.Error.WriteLine(response);
     }
 }
---
+++ Awa/Services/ScreenshotService.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Configuration;
+using Core.Shared;
+using Core.WebsiteArchiveCreator;
+using Domain.Enums;
+using Infrastructure;
+
+[assembly: InternalsVisibleTo("Awa.UnitTests")]
+
+namespace Awa;
+
+internal class ScreenshotService
+{
+    private readonly IScreenshotCreator _screenshotCreator;
+    private readonly HttpService _httpService;
+    
+    public ScreenshotService(IScreenshotCreator screenshotCreator)
+    {
+        _screenshotCreator = screenshotCreator;
+        _httpService = new HttpService();
+    }
+    
+    public async Task<string> TakeScreenshotAsync(string url, string name, string description, bool pdf, bool image, Action runBeforeScreenshot, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var apiKey = await FileSystemService.ReadTextAsync(ConfigurationConstants.FileNameWithApiKey, cancellationToken);
+
+            Stream stream;
+            if (RuntimeInfoService.IsMac())
+            {
+                stream = await _screenshotCreator.TakeScreenshotStreamAsync(url, ArchiveType.Pdf, runBeforeScreenshot, cancellationToken);   
+            }
+            else
+            {
+                stream = await _screenshotCreator.TakeScreenshotStreamAsync(url, ArchiveType.Pdf, cancellationToken);
+            }
+
+            if (!TryGetArchiveType(pdf, image, out var archiveType) || !archiveType.HasValue || !IsValidUrl(url))
+            {
+                await stream.DisposeAsync();
+                return "Error: Choose either PDF or IMAGE.";
+            }
+        
+            var createArchivedWebsiteCommand = new CreateArchivedWebsiteCommand
+            {
+                Name = name,
+                ArchiveType = archiveType ?? ArchiveType.Pdf,
+                Description = description,
+                SourceUrl = url
+            };
+            
+            var result = await _httpService.PostAsync(apiKey, stream, createArchivedWebsiteCommand, cancellationToken);
+            
+            await stream.DisposeAsync();
+            return $"Done. { result }";
+        }
+        catch (Exception)
+        {
+            return "Unspecified error happened. Try again.";
+        }
+    }
+    
+    private bool TryGetArchiveType(bool isPdf, bool isImage, out ArchiveType? archiveType)
+    {
+        if (!isPdf && !isImage)
+        {
+            archiveType = ArchiveType.Pdf;
+            return true;
+        }
+
+        if (isPdf)
+        {
+            archiveType = ArchiveType.Pdf;
+            return true;
+        }
+
+        if (isImage)
+        {
+            archiveType = ArchiveType.Png;
+            return true;
+        }
+        
+        archiveType = null;
+        return false;
+    }
+    
+    private bool IsValidUrl(string url)
+    {
+        string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+        Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        return Rgx.IsMatch(url);
+    }
+}
